123ebf886a809cf1b8243b54eaa595659f933284
Tiago <tiago.ary10@gmail.com>
2023-11-13 09:12:16 -0300

    updating user crud, adding error handling in get all users

--- utils/corsBlocker.py
+++ utils/corsBlocker.py
@@ -3,16 +3,21 @@ import json
 from flask import jsonify, Response
 
 
-def createResponseWithAntiCorsHeaders(data, response_code=None):
-    dump_response = json.dumps(data)
+def createResponseWithAntiCorsHeaders(data, response_code=200):
+    # Converte os dados para JSON
+    dump_response = jsonify(data)
+
+    # Define os cabeçalhos para permitir CORS
     response_headers = {
         "Access-Control-Allow-Origin": "*",
         "Access-Control-Allow-Headers": "*",
         "Access-Control-Allow-Methods": "*"
     }
-    return Response(dump_response, status=response_code, headers=response_headers)
 
-
+    # Cria a resposta com os cabeçalhos e o código de status
+    response = Response(dump_response.data, status=response_code, headers=response_headers)
+    response.mimetype = "application/json"
+    return response
 def getAntiCorsHeaders():
     return {
         "Access-Control-Allow-Origin": "*",
